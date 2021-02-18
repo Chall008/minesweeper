8d4c5290c4b681d1d604ea077895a2e09bff0826
Corey Hall <coreyhall008@gmail.com>
2021-02-18 17:11:01 -0500

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div>
+        <header>Minesweeper</header>
+
+        <footer>
+          <button>Reset Game</button>
+        </footer>
+      </div>
+    )
   }
 }
