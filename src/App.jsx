4049d83b70b93839e5c9eac5c443edac28e15167
Corey Hall <coreyhall008@gmail.com>
2021-02-18 18:23:41 -0500

    Added state for board

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    board: [
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ],
+  }
   render() {
     return (
       <div>
-        <header>Minesweeper</header>
+        <header>
+          <h1>Minesweeper</h1>
+        </header>
 
         <footer>
           <button>Reset Game</button>
